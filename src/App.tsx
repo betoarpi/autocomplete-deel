6009f73ce4778328c9d98f46dcab40e4d048bb01
Rob Arroyo <betoarpi@gmail.com>
2022-09-24 16:40:03 -0500

    Getting list of countries from API call for autocomplete suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useCallback, useEffect, useState } from "react";
+
+const BASE_URL = "https://restcountries.com/v3.1/all";
+
+interface CountryProps {
+  name: { common: string; official: string };
+}
 
 function App() {
+  const [value, setValue] = useState<string>("");
+  const [countriesList, setCountriesList] = useState<string[]>([]);
+
+  const getCountries = useCallback(async () => {
+    const options = {
+      method: "GET",
+    };
+
+    try {
+      const response = await fetch(BASE_URL, options);
+      const data = await response.json();
+
+      const countryList = data.map(
+        (country: CountryProps) => country.name.common
+      );
+
+      setCountriesList(countryList);
+    } catch (error) {
+      console.error("Error getting countries", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getCountries();
+  }, [getCountries]);
+
+  const handleChange = useCallback(
+    (event: React.FormEvent<HTMLInputElement>) => {
+      console.log({ event });
+    },
+    []
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rob Arroyo's Autocomplete</h1>
+
+      <div className="autocomplete-container">
+        <input
+          id="search-input"
+          name="search"
+          type="search"
+          onChange={handleChange}
+          value={value}
+        />
+      </div>
     </div>
   );
 }
